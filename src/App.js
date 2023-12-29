7f4a2980559ec5667971163fff5f9f3de4bacf67
awsderremenperson <selimavuz426@gmail.com>
2023-12-30 05:42:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ExampleWithEffect = () => {
+    const [count, setCount] = useState(0);
 
-export default App;
+    // useEffect будет выполняться после каждого рендеринга компонента
+    useEffect(() => {
+        document.title = `Вы кликнули ${count} раз`;
+    }, [count]); // зависимость, если count изменится, useEffect выполнится заново
+
+    return (
+        <div>
+            <p>Вы кликнули {count} раз</p>
+            <button onClick={() => setCount(count + 1)}>
+                Нажми меня
+            </button>
+        </div>
+    );
+};
+
+export default ExampleWithEffect;
